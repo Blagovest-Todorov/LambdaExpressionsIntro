317326d2ef61bbd93b8ed0f7392ebc787b8f7132
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-22 08:49:54 +0300

    add aProgram.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace LambdaExp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Lambda expresssons anonymous Functions, inline functions, they replace the normal methods to write
+            int[] array = Console.ReadLine()  // no name functions, give built in functionality
+                .Split(" ", StringSplitOptions  // provide better readable code
+                .RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] evenNumbers = array
+                .Where((x, index) =>
+               {
+                   Console.WriteLine($"Checking at Array[{index}] {x} % 2 == 0 -> {x % 2 == 0}");
+                   return x % 2 == 0;  // Predicate- function that returns yes or no
+                })
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", evenNumbers));
+            
+        }
+    }
+}
